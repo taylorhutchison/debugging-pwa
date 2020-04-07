83ef4b4f9a048984ee6946fda8c1892e0a14df92
deekshasharma <deeksha.sharma25@gmail.com>
2020-04-07 08:34:13 -0700

    caching-work in progress - cache-first-then n/w and update

--- src/sw.js
+++ src/sw.js
@@ -29,20 +29,16 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   const cacheName = "version1";
-  console.log("Service Worker intercepting fetch calls");
   if (!navigator.onLine)
     event.respondWith(caches.match(event.request).then((response) => response));
 
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) return response;
-      return caches.open(cacheName).then((cache) => {
-        console.log("cache created");
-        return fetch(event.request).then((networkResponse) => {
-          return caches.open(cacheName).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
         });
       });
     })
