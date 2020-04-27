9b61b397f2b42069b27fb934782fdee0f5120b53
deekshasharma <deeksha.sharma25@gmail.com>
2020-04-27 16:33:36 -0700

    Code for beforeinstallprompt is commented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button } from "@material-ui/core";
+// import { Button } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { About } from "./About";
@@ -6,27 +6,27 @@ import "./App.css";
 import { Stocks } from "./Stocks";
 
 function App() {
-  const onClick = () => {
-    console.log("👍", "butInstall-clicked");
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) return;
-    promptEvent.prompt();
-    promptEvent.userChoice.then((result) => {
-      console.log("👍", "userChoice", result);
-      // Reset the deferred prompt variable, since
-      // prompt() can only be called once.
-      window.deferredPrompt = null;
-      // Hide the install button.
-      document
-        .getElementById("installContainer")
-        .classList.toggle("hidden", true);
-    });
-  };
+  // const onClick = () => {
+  //   console.log("👍", "butInstall-clicked");
+  //   const promptEvent = window.deferredPrompt;
+  //   if (!promptEvent) return;
+  //   promptEvent.prompt();
+  //   promptEvent.userChoice.then((result) => {
+  //     console.log("👍", "userChoice", result);
+  //     // Reset the deferred prompt variable, since
+  //     // prompt() can only be called once.
+  //     window.deferredPrompt = null;
+  //     // Hide the install button.
+  //     document
+  //       .getElementById("installContainer")
+  //       .classList.toggle("hidden", true);
+  //   });
+  // };
 
   return (
     <Router>
       <div className="App">
-        <div id="installContainer" className="hidden">
+        {/* <div id="installContainer" className="hidden">
           <Button
             onClick={onClick}
             variant="outlined"
@@ -35,7 +35,7 @@ function App() {
           >
             INSTALL
           </Button>
-        </div>
+        </div> */}
         <header className="header">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
@@ -53,12 +53,12 @@ function App() {
   );
 }
 
-window.addEventListener("beforeinstallprompt", (event) => {
-  console.log("👍", "beforeinstallprompt", event);
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  // Remove the 'hidden' class from the install button container
-  document.getElementById("installContainer").classList.toggle("hidden", false);
-});
+// window.addEventListener("beforeinstallprompt", (event) => {
+//   console.log("👍", "beforeinstallprompt", event);
+//   // Stash the event so it can be triggered later.
+//   window.deferredPrompt = event;
+//   // Remove the 'hidden' class from the install button container
+//   document.getElementById("installContainer").classList.toggle("hidden", false);
+// });
 
 export default App;
