ff1ee24ee3581ed683b0f4f8268f09e81ea5bc6b
deekshasharma <deeksha.sharma25@gmail.com>
2020-05-18 07:27:46 -0700

    Added alternative approachfor sw-update

--- src/swConfig.js
+++ src/swConfig.js
@@ -1,13 +1,34 @@
+/**
+ * Activate the waiting service worker and refresh the page
+ */
+// export default {
+//   onUpdate: (registration) => {
+//     const waitingServiceWorker = registration.waiting;
+//     if (waitingServiceWorker) {
+//       waitingServiceWorker.addEventListener("statechange", (event) => {
+//         if (event.target.state === "activated") {
+//           window.location.reload();
+//         }
+//       });
+//     }
+//     waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+//   },
+// };
+
+/**
+ * Activate the waiting service worker and inform user to refresh the page
+ */
 export default {
   onUpdate: (registration) => {
     const waitingServiceWorker = registration.waiting;
     if (waitingServiceWorker) {
-      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
       waitingServiceWorker.addEventListener("statechange", (event) => {
         if (event.target.state === "activated") {
           alert("App is updated! Refresh to check new features");
         }
       });
     }
+    waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
   },
 };
+
