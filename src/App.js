80bac2dd80138024e7f5fd66ec19ee8dbb16d84f
deekshasharma <deeksha.sharma25@gmail.com>
2020-05-20 11:58:16 -0700

    Added Offline indicator

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { About } from "./About";
 import "./App.css";
 import { Stocks } from "./Stocks";
+import { Offline } from "./Offline";
 
 function App() {
   const onClick = () => {
@@ -29,6 +30,7 @@ function App() {
             INSTALL
           </Button>
         </div>
+        {!navigator.onLine && <Offline />}
         <header className="header">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
